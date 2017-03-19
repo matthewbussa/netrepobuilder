8bfce667356d289cdda701a20dd6ef2912a3678c
Matthew Bussa <matthewbussa@gmail.com>
2017-03-18 22:26:46 -0400

    robustified a bit

--- repo_builder_app.rb
+++ repo_builder_app.rb
@@ -13,22 +13,49 @@ post '/repo/' do
   repo_name = params[:repo_name]
   email = params[:email]
 
-  if (email.to_s.empty? || !email.end_with?("pillartechnology.com"))
-    flash[:error] = "Email is not accepted"
+  puts "Repo Name #{repo_name}"
+  puts "Email #{email}"
+
+  validation_error_message = validate(repo_name, email)
+
+  if (!validation_error_message.empty?)
+    flash[:error] = validation_error_message
     redirect '/repo/'
   end
 
-  # app = AppVeyor.new(repo_name, email)
-  # response = app.setup_new_build
-
-  success = "Build was sent to AppVeyor Successfully.  An email will be sent once the build is finished"
   error = "Something went wrong"
+  success = "Build was sent to AppVeyor Successfully.  An email will be sent once the build is finished"
 
-  if (response.code == "200")
-    flash[:success] = success
-  else
+  begin
+    app = AppVeyor.new(repo_name, email)
+    response = app.setup_new_build
+
+    if (response.code == "200")
+      flash[:success] = success
+    else
+      flash[:error] = error
+    end
+
+  rescue Exception => e
+    logger.error "Error:  Repo Name:  #{repo_name} and email: #{email} " + e.message
     flash[:error] = error
   end
 
   redirect '/repo/'
+end
+
+def validate(repo_name, email)
+  error_message = ""
+
+  if (repo_name.to_s.empty?)
+    error_message = "Repository Name is empty"
+  elsif (!repo_name.include? '/' )
+    error_message = "Invalid Repository Name"
+  elsif (email.to_s.empty?)
+    error_message = "Email is empty"
+  elsif (!email.to_s.end_with? "pillartechnology.com" )
+    error_message = "Email domain is not accepted"
+  end
+
+  error_message
 end
